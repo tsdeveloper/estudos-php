7721d20cfc7fb822ac694e84f7c8b793a6e6d82d
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com.br>
2020-01-11 11:43:53 -0300

    Ajustado

---
+++ exemplo01.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome = "Azul";
+$modelo = 'Embraer 190';
+
+$matricula = "AF19405";
+
+
+//aspas duplas interpolação de variaveis
+echo "ATR $nome";
+
+echo "<br>";
+
+echo 'ATR $nome';
+
+?>
