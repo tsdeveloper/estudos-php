eeaccfd92a4c5138114b2dfd1a1abbf93633b91f
Build Code <build.code.developer@gmail.com>
2020-02-28 20:12:31 -0300

    02.12 - Constantes e constantes mágicas

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
 <?php
 require __DIR__ . '/fullstackphp/fsphp.php';
-fullStackPHPClassName("02.12 - Constantes e constantes mágicas");
+fullStackPHPClassName('02.12 - Constantes e constantes mágicas');
 
 /*
  * [ constantes ] https://php.net/manual/pt_BR/language.constants.php
  */
-fullStackPHPClassSession("constantes", __LINE__);
+fullStackPHPClassSession('constantes', __LINE__);
 
-define("COURSE", "Full Stack PHP");
-const AUTHOR = "Robson";
+define('COURSE', 'Full Stack PHP');
+const AUTHOR = 'Robson';
 
 $formation = true;
 if ($formation) {
-    define("COURSE_TYPE", "Formação");
+    define('COURSE_TYPE', 'Formação');
 } else {
-    define("COURSE_TYPE", "Curso");
+    define('COURSE_TYPE', 'Curso');
 }
 
-echo "<p>COURSE_TYPE COURSE AUTHOR</p>";
-echo "<p>{COURSE_TYPE} {COURSE} {AUTHOR}</p>";
-echo "<p>", COURSE_TYPE, " ", COURSE, " de ", AUTHOR, "</p>";
-echo "<p>" . COURSE_TYPE . " " . COURSE, " de " . AUTHOR . "</p>";
+echo '<p>COURSE_TYPE COURSE AUTHOR</p>';
+echo '<p>{COURSE_TYPE} {COURSE} {AUTHOR}</p>';
+echo '<p>', COURSE_TYPE, ' ', COURSE, ' de ', AUTHOR, '</p>';
+echo '<p>' . COURSE_TYPE . ' ' . COURSE, ' de ' . AUTHOR . '</p>';
 
 
 class Config
 {
-    const USER = "root";
-    const HOST = "localhost";
+    const USER = 'root';
+    const HOST = 'localhost';
 }
 
-echo "<p>", Config::HOST, "</p>";
-echo "<p>", Config::USER, "</p>";
+echo '<p>', Config::HOST, '</p>';
+echo '<p>', Config::USER, '</p>';
 
 
-var_dump(get_defined_constants(true)["user"]);
+var_dump(get_defined_constants(true)['user']);
 
 /*
  * [ constantes mágicas ] https://php.net/manual/pt_BR/language.constants.predefined.php
  */
-fullStackPHPClassSession("constantes mágicas", __LINE__);
+fullStackPHPClassSession('constantes mágicas', __LINE__);
 
 var_dump([
     __LINE__,
@@ -69,6 +69,6 @@ class FsPHP
 var_dump(new FsPHP());
 
 
-require __DIR__ . "/MyClass.php";
+require __DIR__ . '/MyClass.php';
 var_dump(new \Source\MyClass());
 var_dump(\Source\MyClass::class);
