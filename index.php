d731cadc4d23262463d731b607c33f94e94a6bcd
thiago <ts.analista@gmail.com>
2020-04-24 18:26:01 -0300

    seventh commit

--- index.php
+++ index.php
@@ -7,60 +7,10 @@ $adicionado3 = "Aqui novas alterações do git3";
 /**
  * [ echo ] https://php.net/manual/pt_BR/function.echo.php
  */
-fullStackPHPClassSession("echo", __LINE__);
-
-echo "<p>Olá Mundo!", " ", "<span class='tag'>#BoraProgramar!</span>", "</p>";
-$hello = "Olá mundo!"; //string;
-$code = "<span class='tag'>#BoraProgramar!</span>";
-
-echo "<p>$hello</p>";
-echo '<p>$hello</p>';
-
-$day = "dia";
-echo "<p>Falta 1 $day para o evento</p>";
-echo "<p>Falta 2 {$day} para o evento</p>";
-
-echo '<h3>' . $hello . " " . $code . '</h3>';
-?>
-
-<h4><?php echo $hello;?></h4>
-<h4><?= $hello; ?></h4> 
-
-<?php
+fullStackPHPClassSession("tudo minusculo", __LINE__);
+var_dump("vidraça");
 
 
-/**
- * [ print ] https://php.net/manual/pt_BR/function.print.php
- */
-fullStackPHPClassSession("print", __LINE__);
+fullStackPHPClassSession("tudo maiúsculo", __LINE__);
 
-print $hello;
-print $code;
-/**
- * [ print_r ] https://php.net/manual/pt_BR/function.print-r.php
- */
-fullStackPHPClassSession("print_r", __LINE__);
-$array = [
-    'company' => 'Web Developer',
-    'course' => 'PHP',
-    'class' => 'Comandos de saída',
-];
-print_r($array);
-echo "<pre>", print_r($array), "</pre>";
-
-/**
- * [ printf ] https://php.net/manual/pt_BR/function.printf.php
- */
-fullStackPHPClassSession("printf", __LINE__);
-
-
-/**
- * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
- */
-fullStackPHPClassSession("vprintf", __LINE__);
-
-
-/**
- * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
- */
-fullStackPHPClassSession("var_dump", __LINE__);
+var_dump(strtoupper("vidraça"));
