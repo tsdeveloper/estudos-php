32d7d436d854946e0073b2540c2c30c06c505e3a
thiago <ts.analista@gmail.com>
2020-04-24 17:18:56 -0300

    fix:  = Aqui novas alterações do git3

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require __DIR__ . '/fullstackphp/fsphp.php';
 fullStackPHPClassName("02.03 - Comandos de saída");
 
 $adicionado2 = "Aqui novas alterações do git2";
+$adicionado3 = "Aqui novas alterações do git3";
 /**
  * [ echo ] https://php.net/manual/pt_BR/function.echo.php
  */
