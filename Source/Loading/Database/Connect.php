3fc19066dd62625b51a6e76fdacfb6e370bd9f46
thiago <ts.analista@gmail.com>
2020-06-05 23:02:56 -0300

    Fix:a05.01-connect-pdo

--- Source/Loading/Database/Connect.php
+++ Source/Loading/Database/Connect.php
@@ -1,8 +1,54 @@
 <?php
 namespace Source\Loading\Database;
 
+use PDO;
+use PDOException;
 
 class Connect {
+
+
+    private const HOST = 'localhost';
+    private const USER = 'root';
+    private const DBNAME = 'estudosphp';
+    private const PASSWD = 'root';
+
+    private static $instance;
+
+    private const OPTIONS = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', //Configura para aceitar carateres especiais
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Exibe erros de exception
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, // converter os dados do PDO como object array
+        PDO::ATTR_CASE => PDO::CASE_NATURAL // traz todos nomes de colunas conforme foi criado
+    ];
+
+    final private function  __construct()
+    {
+    }
+
+    final private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    public static function getInstance() {
+
+        if (empty(self::$instance)) {
+
+            try {
+                self::$instance = new PDO(
+                    "mysql:host=" . self::HOST . ";dbname=" . self::DBNAME,
+                    self::USER,
+                    self::PASSWD,
+                    self::OPTIONS
+                    );
+
+            } catch (PDOException $exception) {
+                var_dump($exception);
+            }
+        }
+
+        return self::$instance;
+    }
 
 
 }
