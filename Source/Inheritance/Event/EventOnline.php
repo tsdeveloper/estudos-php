b15d22414a5329e369024fb8b5b5e886f59e85a3
thiago <thiago.almeida@ndd.com.br>
2020-08-22 13:18:07 -0300

    fix: aula_rev_04_08_heranca_polimorfismo

---
+++ Source/Inheritance/Event/EventOnline.php
@@ -0,0 +1,21 @@
+<?php
+namespace Source\Inheritance\Event;
+
+class EventOnline extends Event {
+
+    private $link;
+
+    public function __construct($event, $date, $price, $link, $vacancies = null) {
+        parent::__construct($event, $date, $price, $vacancies);
+        $this->link = $link;
+
+    }
+
+    public function register($fullName, $email) {
+       $this->vacancies += 1;
+       $this->setRegister($fullName, $email);
+       echo "<p class='trigger accept'> Show {$fullName}, cadastrado com sucesso!</p>";
+       
+    }
+
+}
