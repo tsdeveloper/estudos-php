6e9d968ec06686bb6dcd353ab5f56dd055a762b8
Your Name <you@example.com>
2021-09-16 21:59:01 -0300

    fix: a05.10_Model_bootstrap_adastro

--- Source/Loading/Classes/Model.php
+++ Source/Loading/Classes/Model.php
@@ -1,119 +1,191 @@
 <?php
-
 namespace Source\Loading\Classes;
 
 use Source\Loading\Database\Connect;
 
+/**
+ * Class Model
+ * @package Source\Models
+ */
 abstract class Model
 {
     /** @var object|null */
     protected $data;
 
-    /** @var \PDOException */
+    /** @var \PDOException|null */
     protected $fail;
 
     /** @var string|null */
     protected $message;
 
+    /**
+     * @param $name
+     * @param $value
+     */
     public function __set($name, $value)
     {
-        // TODO: Implement __set() method.
         if (empty($this->data)) {
             $this->data = new \stdClass();
         }
 
-        if (is_array($value) && !empty($value)){
-            $this->data->$name = [$value];
-        }else {
-            $this->data->$name = $value;
-        }
+        $this->data->$name = $value;
     }
 
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return isset($this->data->$name);
+    }
+
+    /**
+     * @param $name
+     * @return null
+     */
     public function __get($name)
     {
         return ($this->data->$name ?? null);
     }
 
-    /** @return null|object */
+    /**
+     * @return null|object
+     */
     public function data(): ?object
     {
         return $this->data;
     }
 
-    /** @return \PDOException */
+    /**
+     * @return \PDOException
+     */
     public function fail(): ?\PDOException
     {
         return $this->fail;
     }
 
-    /** @return null|string */
+    /**
+     * @return null|string
+     */
     public function message(): ?string
     {
         return $this->message;
     }
 
-    protected function create()
-    {
-    }
-
-    protected function update()
-    {
-    }
-
-    protected function delete()
-    {
-    }
-
-    protected function read(string $select, string $params = null): ?\PDOStatement
+    /**
+     * @param string $entity
+     * @param array $data
+     * @return int|null
+     */
+    protected function create(string $entity, array $data): ?int
     {
         try {
+            $columns = implode(", ", array_keys($data));
+            $values = ":" . implode(", :", array_keys($data));
 
-            parse_str($params, $strOptionWhere);
+            $stmt = Connect::getInstance()->prepare("INSERT INTO {$entity} ({$columns}) VALUES ({$values})");
+            $stmt->execute($this->filter($data));
 
-            $where = null;
-            foreach ($strOptionWhere as $key => $value){
-
-                if (empty($where)) {
-                    $where  = "{$key} = :{$key}";
-                }else if(count($strOptionWhere) > 1)
-                    $where  .= "{$strOptionWhere} {$key} = :{$key}";
-            }
-
-            $stm = Connect::getInstance()->prepare($select);
-
-            if ($params) {
-
-                echo '$params antes da conversão';
-
-                parse_str($params, $params);
-
-                echo '$params depois da conversão';
-
-                foreach ($params as $key => $value) {
-
-
-                    $type = (is_numeric($value) ? \PDO::PARAM_INT : \PDO::PARAM_STR);
-
-                    $stm->bindValue(":{$key}", $value, $type);
-                }
-            }
-
-            $stm->execute();
-            return $stm;
-
+            return Connect::getInstance()->lastInsertId();
         } catch (\PDOException $exception) {
             $this->fail = $exception;
             return null;
         }
     }
 
+    /**
+     * @param string $select
+     * @param string|null $params
+     * @return null|\PDOStatement
+     */
+    protected function read(string $select, string $params = null): ?\PDOStatement
+    {
+        try {
+            $stmt = Connect::getInstance()->prepare($select);
+            if ($params) {
+                parse_str($params, $params);
+                foreach ($params as $key => $value) {
+                    $type = (is_numeric($value) ? \PDO::PARAM_INT : \PDO::PARAM_STR);
+                    $stmt->bindValue(":{$key}", $value, $type);
+                }
+            }
+
+            $stmt->execute();
+            return $stmt;
+        } catch (\PDOException $exception) {
+            $this->fail = $exception;
+            return null;
+        }
+    }
+
+    /**
+     * @param string $entity
+     * @param array $data
+     * @param string $terms
+     * @param string $params
+     * @return int|null
+     */
+    protected function update(string $entity, array $data, string $terms, string $params): ?int
+    {
+        try {
+            $dateSet = [];
+            foreach ($data as $bind => $value) {
+                $dateSet[] = "{$bind} = :{$bind}";
+            }
+            $dateSet = implode(", ", $dateSet);
+            parse_str($params, $params);
+
+            $stmt = Connect::getInstance()->prepare("UPDATE {$entity} SET {$dateSet} WHERE {$terms}");
+            $stmt->execute($this->filter(array_merge($data, $params)));
+            return ($stmt->rowCount() ?? 1);
+        } catch (\PDOException $exception) {
+            $this->fail = $exception;
+            return null;
+        }
+    }
+
+    /**
+     * @param string $entity
+     * @param string $terms
+     * @param string $params
+     * @return int|null
+     */
+    protected function delete(string $entity, string $terms, string $params): ?int
+    {
+        try {
+            $stmt = Connect::getInstance()->prepare("DELETE FROM {$entity} WHERE {$terms}");
+            parse_str($params, $params);
+            $stmt->execute($params);
+            return ($stmt->rowCount() ?? 1);
+        } catch (\PDOException $exception) {
+            $this->fail = $exception;
+            return null;
+        }
+    }
+
+    /**
+     * @return array|null
+     */
     protected function safe(): ?array
     {
+        $safe = (array)$this->data;
+        foreach (static::$safe as $unset) {
+            unset($safe[$unset]);
+        }
+        return $safe;
     }
 
-    private function filter()
+    /**
+     * @param array $data
+     * @return array|null
+     */
+    protected function filter(array $data)
     {
+        $filter = [];
+        foreach ($data as $key => $value) {
+            $filter[$key] = (is_null($value) ? null : filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS));
+        }
+        var_dump($filter);
     }
-
 }
-
--- Source/Loading/Classes/UserModel.php
+++ Source/Loading/Classes/UserModel.php
@@ -1,54 +1,58 @@
 <?php
 
-
 namespace Source\Loading\Classes;
 
 
 use http\Client\Curl\User;
 
+/**
+ * Class UserModel
+ * @package Source\Models
+ */
 class UserModel extends Model
 {
     /** @var array $safe no update or create */
-    protected static $safe = ['id', 'created_at', 'updated_at'];
+    protected static $safe = ["id", "created_at", "updated_at"];
 
     /** @var string $entity database table */
-    protected static $entity = 'users';
+    protected static $entity = "users";
 
     /**
-     * @return string
+     * @param string $firstName
+     * @param string $lastName
+     * @param string $email
+     * @param string|null $document
+     * @return null|UserModel
      */
-    public  function bootstrap($first_name, $last_name, $email, $document): ?UserModel
+    public function bootstrap(string $firstName, string $lastName, string $email, string $document = null): ?UserModel
     {
-        $this->first_name = $first_name;
-        $this->last_name = $last_name;
+        $this->first_name = $firstName;
+        $this->last_name = $lastName;
         $this->email = $email;
         $this->document = $document;
-
         return $this;
     }
 
-    public function  load(string $params,  string $columns = "*", string $operadorWhere = " AND " ): ?UserModel {
-
-        parse_str($params, $strOptionWhere);
-
-        $where = null;
-        foreach ($strOptionWhere as $key => $value){
-
-            if (empty($where)) {
-                $where  = "{$key} = :{$key}";
-            }else if(count($strOptionWhere) > 1)
-                    $where  .= "{$operadorWhere} {$key} = :{$key}";
+    /**
+     * @param int $id
+     * @param string $columns
+     * @return null|UserModel
+     */
+    public function load(int $id, string $columns = "*"): ?UserModel
+    {
+        $load = $this->read("SELECT {$columns} FROM " . self::$entity . " WHERE id = :id", "id={$id}");
+        if ($this->fail() || !$load->rowCount()) {
+            $this->message = "Usuário não encontrado para o id informado";
+            return null;
         }
-            $load = $this->read("SELECT {$columns} FROM " . self::$entity .  " where {$where}"
-            ,$params);
-
-            if ($this->fail() || !$load->rowCount()) {
-                $this->message = "Usuário não encontrado para o id informado!";
-                return null;
-            }
-          return  $load->fetchObject(__CLASS__);
+        return $load->fetchObject(__CLASS__);
     }
 
+    /**
+     * @param $email
+     * @param string $columns
+     * @return null|UserModel
+     */
     public function find($email, string $columns = "*"): ?UserModel
     {
         $find = $this->read("SELECT {$columns} FROM " . self::$entity . " WHERE email = :email", "email={$email}");
@@ -59,11 +63,64 @@ class UserModel extends Model
         return $find->fetchObject(__CLASS__);
     }
 
-    public function save() {
-
+    /**
+     * @param int $limit
+     * @param int $offset
+     * @param string $columns
+     * @return array|null
+     */
+    public function all(int $limit = 30, int $offset = 0, string $columns = "*"): ?array
+    {
+        $all = $this->read("SELECT {$columns} FROM " . self::$entity . " LIMIT :l OFFSET :o", "l={$limit}&o={$offset}");
+        if ($this->fail() || !$all->rowCount()) {
+            $this->message = "Sua consulta não retornou usuários";
+            return null;
+        }
+        return $all->fetchAll(\PDO::FETCH_CLASS, __CLASS__);
     }
 
+    /**
+     * @return null|UserModel
+     */
+    public function save()
+    {
+        $this->filter($this->safe());
+    }
 
+    /**
+     * @return null|UserModel
+     */
+    public function destroy(): ?UserModel
+    {
+        if (!empty($this->id)) {
+            $this->delete(self::$entity, "id = :id", "id={$this->id}");
+        }
 
+        if ($this->fail()) {
+            $this->message = "Não foi possível remover o usuário";
+            return null;
+        }
 
+        $this->message = "Usuário removido com sucesso";
+        $this->data = null;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    private function required(): bool
+    {
+        if (empty($this->first_name) || empty($this->last_name) || empty($this->email)) {
+            $this->message = "Nome, sobrenome e e-mail são obrigatórios";
+            return false;
+        }
+
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $this->message = "O e-mail informado não parece válido";
+            return false;
+        }
+
+        return true;
+    }
 }
