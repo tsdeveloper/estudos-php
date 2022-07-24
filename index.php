6f00eb5ea7a8b15c988d834ca37497032a523375
Web Developer <ts.analista@gmail.com>
2022-07-24 11:19:55 -0300

    fix: 07.a08-template-engine-plates

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use source\Core\Email;
 use source\Models\User;
 
 require __DIR__ . '/fullstackphp/fsphp.php';
-fullStackPHPClassName("07.a06-sintetizando-e-abstraindo");
+fullStackPHPClassName("07.a08-template-engine-plates");
 //echo '<pre>';
 /*
  * [ classe e objeto ] http://php.net/manual/pt_BR/language.oop5.basic.php
@@ -23,10 +23,29 @@ echo '<pre>';
 
 try {
                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-$user =
+// Create new Plates instance
+$templates = new League\Plates\Engine(__DIR__. '/assets/views');
+
+
+$templates->addFolder("test", "test");
+
+//Render a template
+if (empty($_GET["id"])){
+    echo $templates->render('test::list', [
+        "title" => "Usuários do Sistema",
+        'list' => (user()->all(3))
+    ]);
+}
+else {
+    echo $templates->render('test::user', [
+        "title" => "Usuários do Sistema",
+        'list' => (user()->find($_GET["id"]))
+    ]);
+}
+
 
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "Message could not be sent. Mailer Error: {$e}";
 }
 
 echo '</pre>';
---
+++ templates/profile.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body>
+
+<header>
+    <h3 class="trigger accept"><?= '$title'; ?></h3>
+</header>
+
+</body>
+</html>
