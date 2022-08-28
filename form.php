00c86e6d745af96761f452f44dae8e88cb2a91fa
Web Developer <ts.analista@gmail.com>
2022-08-28 11:21:14 -0300

    fix: 07.a09-package-cropper

--- form.php
+++ form.php
@@ -17,38 +17,6 @@
     <button class="green"><?= $formSend; ?></button>
 </form>
 
-<form action="./" method="post" enctype="multipart/form-data" novalidate>
-    <?= ($error ?? ""), csrf_input(); ?>
-    <p>First name: <input type="text" name="first_name" /></p>
-    <p>Last name: <input type="text" name="last_name" /></p>
-    <p>Email: <input type="email" name="email" /></p>
-    <p>Password: <input type="password" name="password" /></p>
-    <button>Save</button>
-</form>
-
-<script src="./node_modules/jquery/dist/jquery.min.js"></script>
-<script src="./node_modules/sweetalert/dist/sweetalert.min.js"></script>
-<script>
-
-    $(function () {
-        /*  swal({
-                  title: "Are you sure?",
-                  text: "Once deleted, you will not be able to recover this imaginary file!",
-                  icon: "warning",
-                  buttons: true,
-                  dangerMode: true,
-              })
-              .then((willDelete) => {
-                  if (willDelete) {
-                      swal("Poof! Your imaginary file has been deleted!", {
-                          icon: "success",
-                      });
-                  } else {
-                      swal("Your imaginary file is safe!");
-                  }
-              });*/
-    })
-</script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -16,38 +16,19 @@ fullStackPHPClassName("07.a10-template-engine-plates");
 //Obrigatorio
 
 require __DIR__ . '/vendor/autoload.php';
+$formSend = "image";
+require __DIR__ . "/form.php";
 
 echo '<pre>';
-
-//Create an instance; passing `true` enables exceptions
-
-try {
-                                 //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-// Create new Plates instance
-$view = view()->path("test", "test");
-
-
-//[10,20,50  e 100]
-//Render a template
-if (empty($_GET["id"])){
-    echo $view->render('test::list', [
-        "title" => "Usuários do Sistema",
-        'listUsuarios' => (user()->all(3))
-    ]);
-}
-else {
-    echo $view->render('test::user', [
-        "title" => "Usuários do Sistema",
-        'user' => (user()->find("id != :v", "v={$_GET["id"]}"))
-    ]);
-}
-
-
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$e}";
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+var_dump($post['send']);
+if ($post && $post['send'] == "image")
+{
+    var_dump($post, ($_FILES ?? ""));
 }
 
 echo '</pre>';
+$formSend = "file";
 require __DIR__ . "/form.php";
 
 ?>
